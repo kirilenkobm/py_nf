9763afd95971db3d769823710bc28ed6eb14fb6c
kirilenkobm <kirilenkobm@gmail.com>
2020-09-19 17:42:59 +0300

    improved setup py

--- setup.py
+++ setup.py
@@ -1,21 +1,33 @@
 #!/usr/bin/env python3
 """Setup module.
 
-Was not properly tested!
+A way to install this:
+python3 setup.py bdist_wheel
+pip3 install dist/py_nf-0.1.0-py3-none-any.whl
 """
 from setuptools import find_packages, setup
 
 with open("README.md", "r") as f:
     long_description = f.read()
 
+
+classifiers=[
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: POSIX",
+    "Development Status :: 2 - Pre-Alpha",
+    "Topic :: Scientific/Engineering :: Bio-Informatics"
+]
+
 setup(name="py_nf",
       packages=find_packages(include=["py_nf"]),
       version="0.1.0",
-      description="Library to execute job lists with nextflow",
+      description="Execute batches of jobs with nextflow",
       long_description=long_description,
       long_description_content_type="text/markdown",
       url="https://github.com/kirilenkobm/py_nf",
       author="Bogdan Kirilenko",
       author_email="kirilenkobm@gmail.com",
       license="MIT",
+      classifiers=classifiers,
       )
