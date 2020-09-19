8aac81672f64a254441221660b19bd0eaa8ec36a
kirilenkobm <kirilenkobm@gmail.com>
2020-09-19 19:42:44 +0300

    bit more tests

--- tests/test_py_nf.py
+++ tests/test_py_nf.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 """Test nextflow wrapper."""
+# TODO: do with pytest
 import sys
 import os
+import shutil
 import pytest
 # a temporary solution for import error:
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
@@ -33,6 +35,9 @@ def get_joblist(sample_num):
         os.mkdir(out_dir) if not os.path.isdir(out_dir) else None
         to_compare = list(zip(out_files, ref_result_files))
         return jobs, to_compare
+    if sample_num == 2:
+        jobs = [f"not_a_script.py in/{x}.txt out/{x}.txt" for x in range(10)]
+        return jobs, None
     else:
         raise ValueError(f"Test number {sample_num} doesn't exist")
 
@@ -55,8 +60,14 @@ def have_same_content(files_list):
 
 if __name__ == "__main__":
     project_name_1 = "test_project_1"
-    exe = pick_executor()
-    nf_instance = Nextflow(project_name=project_name_1, executor="slurm", switch_to_local=True)
+    project_name_2 = "test_project_2"
+    if "clean" in sys.argv:
+        projects = [project_name_1, project_name_2]
+        for project in projects:
+            shutil.rmtree(project) if os.path.isdir(project) else None
+        sys.exit("Cleaned")
+    print("Running test 1")
+    nf_instance = Nextflow(project_name=project_name_1, executor="slurm", switch_to_local=True, verbose=True)
     joblist, to_check = get_joblist(1)
     abs_joblist = paths_to_abspaths_in_joblist(joblist)
     status = nf_instance.execute(abs_joblist)
@@ -65,3 +76,32 @@ if __name__ == "__main__":
     assert(status == 0)
     # check that results are correct:
     assert(have_same_content(to_check))
+    del nf_instance
+    print("Running test 2 -> should fail")
+    project_name_1 = "test_project_2"
+    exe = pick_executor()
+    nf_instance = Nextflow(project_name=project_name_2, executor=exe, switch_to_local=True, verbose=True, max_retries=0)
+    joblist_2, _ = get_joblist(2)
+    status = nf_instance.execute(joblist_2)
+    assert(status != 0)
+    print("Test 2: success (NF pipeline should fail)")
+    del nf_instance
+    print("Running test 3 -> should fail")
+    try:
+        nf_instance = Nextflow(project_name=None, executor="non_existent")
+    except NotImplementedError as err:
+        print(err)
+        print("Test 3: OK")
+    else:
+        print("Test 3 -> not raised an error -> failed")
+        sys.exit(1)
+    
+    print("Running test 4 (should fail)")
+    try:
+        nf_instance = Nextflow(project_name=None, executor="lsf")
+    except ValueError as err:
+        print(err)
+        print("Test 4: OK")
+    else:
+        print("Test 4: Fail")
+        sys.exit(1)
